b23a53dad42e8d98dc50901c8ad7e0d84339a42b
Andres Maneiro <amaneiro7@gmail.com>
2025-01-31 16:35:13 -0400

    history

--- src/modules/history/domain/history.ts
+++ src/modules/history/domain/history.ts
@@ -1,5 +1,5 @@
 import { type DeviceId } from '../../devices/devices/devices/domain/DeviceId'
-import { EmployeeId } from '../../employee/employee/domain/EmployeeId'
+import { type EmployeeId } from '../../employee/employee/domain/EmployeeId'
 import { type Primitives } from '../../shared/domain/value-object/Primitives'
 import { type UserId } from '../../user/user/domain/UserId'
 import { type HistoryId } from './HistoryId'
